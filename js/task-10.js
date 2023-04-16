dc67f4c9fcf1d7d163baf8690b45b494704162bc
Serzh1006 <serzh.kravchenko.9696@mail.ru>
2023-04-16 21:33:26 +0200

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const refs = {
+  primaryBox: document.querySelector("#boxes"),
+  inputValue: document.querySelector("#controls"),
+  btnCreateCollections: document.querySelector("[data-create]"),
+  btnDestroyCollections: document.querySelector("[data-destroy]"),
+};
+
+refs.btnCreateCollections.addEventListener("click", onClickCreateCollection);
+refs.btnDestroyCollections.addEventListener("click", onClickDestroyCollection);
+function onClickCreateCollection() {
+  const collection = [];
+  const counter = refs.inputValue.firstElementChild;
+  let increment = 30;
+  for (let i = 0; i < counter.value; i += 1) {
+    const newElem = document.createElement("div");
+    newElem.style.width = `${increment}px`;
+    newElem.style.height = `${increment}px`;
+    const colorDiv = getRandomHexColor();
+    newElem.style.backgroundColor = colorDiv;
+    collection.push(newElem);
+    increment += 10;
+  }
+  refs.primaryBox.append(...collection);
+}
+
+function onClickDestroyCollection() {
+  refs.primaryBox.innerHTML = "";
+}
