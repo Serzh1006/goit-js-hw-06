047ecb1d2251b16f5e9c91d7c099de33ecec5c7d
Serzh1006 <serzh.kravchenko.9696@mail.ru>
2023-04-16 11:01:53 +0200

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector(".login-form");
 
+const onFormSubmit = (event) => {
+  event.preventDefault();
+  if (formEl.elements[0].value === "" || formEl.elements[1].value === "") {
+    alert("Заполните все поля");
+  } else {
+    const emailName = event.currentTarget.elements.email.name;
+    const passwordName = event.currentTarget.elements.password.name;
+    const emailValue = event.currentTarget.email.value;
+    const passwordValue = event.currentTarget.password.value;
+    const registrationDataUser = {
+      [emailName]: emailValue,
+      [passwordName]: passwordValue,
+    };
+    console.log(registrationDataUser);
+    event.currentTarget.reset();
+  }
+};
+
+formEl.addEventListener("submit", onFormSubmit);
