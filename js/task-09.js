246ce7b1ba15e1f5b74c9c3c09df33100389ac6c
Serzh1006 <serzh.kravchenko.9696@mail.ru>
2023-04-16 11:36:44 +0200

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnChangeColor = document.querySelector(".change-color");
+const valueColorInSpan = document.querySelector(".color");
+
+const onClickChangeColorBody = () => {
+  const getbackgColor = getRandomHexColor();
+  document.body.style.backgroundColor = getbackgColor;
+  valueColorInSpan.textContent = getbackgColor;
+};
+
+btnChangeColor.addEventListener("click", onClickChangeColorBody);
